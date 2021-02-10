a82d5b6b5c1069d8a315c810e5bb5f757d92eb42
online135 <b97b01067@ntu.edu.tw>
2021-02-10 10:52:45 +0800

    image preview

--- resources/views/product/create.blade.php
+++ resources/views/product/create.blade.php
@@ -6,20 +6,51 @@
     @csrf
     <div>
         <label>
-            Product name: <input type="text" name="name" value="{{ old('product_name') }}"/>
+            Product name: <input type="text" name="product_name" value="{{ old('product_name') }}"/>
         </label>
     </div>
     <br />
     <div>
         <label>
-            Product price: <input type="number" min=0 name="price" value="{{ old('product_price') }}"/>
+            Product price: <input type="number" min=0 name="product_price" value="{{ old('product_price') }}"/>
         </label>
     </div>
     <br />
     <div>
         <label>
-            Product image: <input type="file" name="image"/>
+            Product image:
+                <input
+                    type="file"
+                    name="product_image"
+                    data-target="preview_product_image"
+                />
         </label>
+        <br/>
+        <div>
+            <img
+                style="max-width: 400px"
+                id="preview_product_image"
+                src="https://via.placeholder.com/400x300"
+                />
+        </div>
+        <script>
+
+        var input = document.querySelector('input[name=product_image]')
+        input.addEventListener('change', function(e) {
+            readURL(e.target)
+        })
+        function readURL(input) {
+            var imgId = input.getAttribute('data-target')
+            var img = document.querySelector('#' + imgId)
+            if (input.files && input.files[0]) {
+                var reader = new FileReader();
+                reader.onload = function (e) {
+                    img.setAttribute('src', e.target.result);
+                }
+                reader.readAsDataURL(input.files[0]);
+            }
+        }
+        </script>
     </div>
     <br />
     <div>
