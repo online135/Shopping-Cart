09b2a6cfd0757d4769a23255290430ec1700c243
online135 <b97b01067@ntu.edu.tw>
2021-02-15 17:19:09 +0800

    is_admin or not

--- app/Http/Controllers/Controls/PageController.php
+++ app/Http/Controllers/Controls/PageController.php
@@ -4,10 +4,19 @@ namespace App\Http\Controllers\Controls;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class PageController extends Controller
 {
-    public function home(){
+    public function home(Request $request){
+
+        $user = Auth::user();
+        $user->update($request->query());
+
+        if (!Auth::user()->is_admin){
+            return redirect('/')->withErrors('你沒有權限');
+        }
+
         return view('controls.home');
     }
 }
